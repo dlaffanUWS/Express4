3b6ad0bb4fe76340f8da4056d3af2db0637b7fc7
dlaffanUWS <B00346084@studentmail.uws.ac.uk>
2022-11-22 14:22:28 +0000

    Database server not working, User-facing server is

--- myapp/app.js
+++ myapp/app.js
@@ -9,7 +9,16 @@ var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
-
+//Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://root:example@mongodb:27017';
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoDB, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
